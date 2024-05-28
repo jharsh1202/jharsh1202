55d031b8369463d1fc273c2c0abfad91f3bd882e
“Harshit <harshit120299@gmail.com>
2024-05-28 22:26:48 +0530

    Set up LeetCode stats integration

--- update_readme.py
+++ update_readme.py
@@ -7,31 +7,13 @@ username = "harshit120299"
 response = requests.get(f"https://leetcode-stats-api.herokuapp.com/{username}")
 data = response.json()
 
-# Update README with stats
-total_solved = data['totalSolved']
-easy_solved = data['easySolved']
-medium_solved = data['mediumSolved']
-hard_solved = data['hardSolved']
+# Simulated activity data for testing
+activity_data = {
+    (datetime.now() - timedelta(days=i)).strftime('%Y-%m-%d'): i % 5
+    for i in range(365)
+}
 
-new_readme_content = f"""
-# LeetCode Stats
-
-![LeetCode Stats](https://leetcode-stats-api.herokuapp.com/{username})
-
-## Solved Problems
-- Total: {total_solved}
-- Easy: {easy_solved}
-- Medium: {medium_solved}
-- Hard: {hard_solved}
-
-## Activity Calendar
-![LeetCode Activity](./leetcode_activity.svg)
-"""
-
-with open("README.md", "w") as f:
-    f.write(new_readme_content)
-
-# Generate SVG calendar
+# Generate SVG calendar (simple example)
 def generate_svg(data):
     svg_header = '<svg width="720" height="110" xmlns="http://www.w3.org/2000/svg">'
     svg_footer = '</svg>'
@@ -52,17 +34,18 @@ def generate_svg(data):
         if solved_count > 0:
             color = "#c6e48b"  # Example color for solved problems
 
-        x = x_offset + (i % 7) * (day_width + day_padding)
-        y = y_offset + (i // 7) * (day_height + day_padding)
+        x = x_offset + (i % 52) * (day_width + day_padding)
+        y = y_offset + (i // 52) * (day_height + day_padding)
 
         svg_body += f'<rect x="{x}" y="{y}" width="{day_width}" height="{day_height}" fill="{color}" />'
 
     return svg_header + svg_body + svg_footer
 
-svg_content = generate_svg(data)
+svg_content = generate_svg(activity_data)
 
 with open("leetcode_activity.svg", "w") as f:
     f.write(svg_content)
+
 
 # import json
 # from datetime import datetime
