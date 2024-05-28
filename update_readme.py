fd07e24f52eee0eef682fe14bd3ba6b74dba91a9
“Harshit <harshit120299@gmail.com>
2024-05-28 22:29:15 +0530

    Set up LeetCode stats integration

--- update_readme.py
+++ update_readme.py
@@ -7,13 +7,31 @@ username = "harshit120299"
 response = requests.get(f"https://leetcode-stats-api.herokuapp.com/{username}")
 data = response.json()
 
-# Simulated activity data for testing
-activity_data = {
-    (datetime.now() - timedelta(days=i)).strftime('%Y-%m-%d'): i % 5
-    for i in range(365)
-}
+# Update README with stats
+total_solved = data['totalSolved']
+easy_solved = data['easySolved']
+medium_solved = data['mediumSolved']
+hard_solved = data['hardSolved']
 
-# Generate SVG calendar (simple example)
+new_readme_content = f"""
+# LeetCode Stats
+
+![LeetCode Stats](https://leetcode-stats-api.herokuapp.com/{username})
+
+## Solved Problems
+- Total: {total_solved}
+- Easy: {easy_solved}
+- Medium: {medium_solved}
+- Hard: {hard_solved}
+
+## Activity Calendar
+![LeetCode Activity](./leetcode_activity.svg)
+"""
+
+with open("README.md", "w") as f:
+    f.write(new_readme_content)
+
+# Generate SVG calendar
 def generate_svg(data):
     svg_header = '<svg width="720" height="110" xmlns="http://www.w3.org/2000/svg">'
     svg_footer = '</svg>'
@@ -41,41 +59,12 @@ def generate_svg(data):
 
     return svg_header + svg_body + svg_footer
 
+activity_data = {
+    (datetime.now() - timedelta(days=i)).strftime('%Y-%m-%d'): i % 5
+    for i in range(365)
+}
+
 svg_content = generate_svg(activity_data)
 
 with open("leetcode_activity.svg", "w") as f:
     f.write(svg_content)
-
-
-# import json
-# from datetime import datetime
-
-# # Read LeetCode stats from JSON file
-# with open('leetcode_stats.json', 'r') as f:
-#     data = json.load(f)
-
-# total_solved = data['totalSolved']
-# easy_solved = data['easySolved']
-# medium_solved = data['mediumSolved']
-# hard_solved = data['hardSolved']
-
-# # Update README file
-# with open('README.md', 'r') as f:
-#     readme_content = f.read()
-
-# new_readme_content = f"""
-# # LeetCode Stats
-
-# ![LeetCode Stats](https://leetcode-stats-api.herokuapp.com/harshit120299)
-
-# ## Solved Problems
-# - Total: {total_solved}
-# - Easy: {easy_solved}
-# - Medium: {medium_solved}
-# - Hard: {hard_solved}
-
-# {readme_content}
-# """
-
-# with open('README.md', 'w') as f:
-#     f.write(new_readme_content)
