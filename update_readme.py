213da9bd9d89159e7910a72f2f6e2a2037c3ddb4
“Harshit <harshit120299@gmail.com>
2024-05-29 11:04:13 +0530

    DSA progress bar with 0 status hardcoded

--- update_readme.py
+++ update_readme.py
@@ -66,6 +66,24 @@ def update_readme(username, svg_content):
 
 ![LeetCode Activity](./leetcode_activity.svg)
 #### Total: {total_solved} | Easy: {easy_solved} | Medium: {medium_solved} | Hard: {hard_solved}
+## Data Structures
+![Progress](https://progress-bar.dev/0/?title=Arrays)
+![Progress](https://progress-bar.dev/0/?title=Linked%20Lists)
+![Progress](https://progress-bar.dev/0/?title=Stacks)
+![Progress](https://progress-bar.dev/0/?title=Queues)
+![Progress](https://progress-bar.dev/0/?title=Trees)
+![Progress](https://progress-bar.dev/0/?title=Graphs)
+![Progress](https://progress-bar.dev/0/?title=Hash%20Tables)
+
+## Algorithms
+![Progress](https://progress-bar.dev/5/?title=Sorting)
+![Progress](https://progress-bar.dev/5/?title=Searching)
+![Progress](https://progress-bar.dev/0/?title=Dynamic%20Programming)
+![Progress](https://progress-bar.dev/0/?title=Greedy%20Algorithms)
+![Progress](https://progress-bar.dev/0/?title=Backtracking)
+![Progress](https://progress-bar.dev/0/?title=Divide%20and%20Conquer)
+![Progress](https://progress-bar.dev/0/?title=Bit%20Manipulation)
+![Progress](https://progress-bar.dev/0/?title=Graph%20Algorithms)
 """
 
     with open("README.md", "w") as f:
